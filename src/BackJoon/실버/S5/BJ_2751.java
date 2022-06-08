7fa248c7789389ffd90574c3d94b4085c8bd2989
서요셉 <tjdytpq0310@gmail.com>
2022-06-08 15:48:29 +0900

    :sparkles: BJ 2751

---
+++ src/BackJoon/실버/S5/BJ_2751.java
@@ -0,0 +1,29 @@
+package BackJoon.실버.S5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BJ_2751 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for (int value : list) {
+            sb.append(value).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
