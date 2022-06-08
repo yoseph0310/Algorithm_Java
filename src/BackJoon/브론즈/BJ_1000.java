8cc2ca5bc6b3a9e5f7b6f48b020ae27d69c32c38
서요셉 <tjdytpq0310@gmail.com>
2022-06-08 14:13:53 +0900

    :sparkles: BJ 1000 A+B

---
+++ src/BackJoon/브론즈/BJ_1000.java
@@ -0,0 +1,22 @@
+package BackJoon.브론즈;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_1000 {
+
+    static int A, B;
+    static int sum;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        sum = A + B;
+
+        System.out.println(sum);
+    }
+}
