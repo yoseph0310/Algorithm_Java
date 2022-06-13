316a932f957f4d766b7daaa01f097808cef0ed49
서요셉 <tjdytpq0310@gmail.com>
2022-06-13 23:07:23 +0900

    :recycle: BJ 10989

--- src/BackJoon/브론즈/B1/BJ_10989.java
+++ src/BackJoon/브론즈/B1/BJ_10989.java
@@ -9,7 +9,7 @@ public class BJ_10989 {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int [] arr = new int[10000001];
+        int [] arr = new int[10001];
 
         for (int i = 0; i < N; i++) {
             arr[Integer.parseInt(br.readLine())] += 1;
