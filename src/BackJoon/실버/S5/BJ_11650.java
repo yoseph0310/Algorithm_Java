26bfd42935df218dc5bfb8def83337b275d23242
Yoseph <tjdytpq0310@gmail.com>
2022-06-20 16:55:19 +0900

    :sparkles: BJ 11650

---
+++ src/BackJoon/실버/S5/BJ_11650.java
@@ -0,0 +1,40 @@
+package BackJoon.실버.S5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_11650 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int [][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr,(e1, e2) -> {
+            if(e1[0] == e2[0]){
+                return e1[1] - e2[1];
+            }
+            else{
+                return e1[0] - e2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
