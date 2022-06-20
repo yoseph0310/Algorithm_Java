852f96a3285e495c01492b06e5a459705cd68fb0
Yoseph <tjdytpq0310@gmail.com>
2022-06-20 17:33:23 +0900

    :sparkles: BJ 10867 HashSet 사용

---
+++ src/BackJoon/실버/S5/BJ_10867.java
@@ -0,0 +1,35 @@
+package BackJoon.실버.S5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class BJ_10867 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> hs = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            hs.add(Integer.parseInt(st.nextToken()));
+        }
+
+        ArrayList<Integer> al = new ArrayList<>(hs);
+        Collections.sort(al);
+
+        for (int num: al) {
+            bw.write(num+" ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
