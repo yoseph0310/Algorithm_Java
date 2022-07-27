7f0b46bea371ef41442bc62567e1ee83e6301db0
Yoseph <tjdytpq0310@gmail.com>
2022-07-27 19:30:52 +0900

    :sparkles: 직사각형 별찍기

---
+++ src/Programmers/LV1/직사각형별찍기.java
@@ -0,0 +1,22 @@
+package Programmers.LV1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 직사각형별찍기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
