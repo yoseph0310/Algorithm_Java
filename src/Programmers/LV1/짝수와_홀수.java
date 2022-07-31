8514c3522a994513808ac8d3b4787d78e41e819a
서요셉 <tjdytpq0310@gmail.com>
2022-07-31 16:07:21 +0900

    :sparkles: 짝수와 홀수

---
+++ src/Programmers/LV1/짝수와_홀수.java
@@ -0,0 +1,11 @@
+package Programmers.LV1;
+
+public class 짝수와_홀수 {
+    public String solution(int num) {
+        String answer = "";
+
+        answer = (num % 2 == 0) ? "Even" : "Odd";
+
+        return answer;
+    }
+}
