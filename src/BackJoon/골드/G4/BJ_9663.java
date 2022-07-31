c84299386d9881b8944c5f2029afc43d9cd36664
서요셉 <tjdytpq0310@gmail.com>
2022-07-31 18:05:59 +0900

    :sparkles: BJ 9663

---
+++ src/BackJoon/골드/G4/BJ_9663.java
@@ -0,0 +1,49 @@
+package BackJoon.골드.G4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BJ_9663 {
+
+    static int N;
+    static int cnt = 0;
+    static int [] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        nQueen(0);
+        bw.write(cnt+"");
+        bw.flush();
+        bw.close();
+    }
+
+    public static void nQueen(int depth){
+        if(depth == N){
+            cnt++;
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i;
+            if(Possibility(depth)){
+                nQueen(depth+1);
+            }
+        }
+    }
+    public static boolean Possibility(int col){
+        for (int i = 0; i < col; i++) {
+            if(arr[col] == arr[i]){
+                return false;
+            }
+            else if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
