9cd1a256f12405f1615271d56aa6ddf45935afa3
서요셉 <tjdytpq0310@gmail.com>
2022-08-15 19:45:00 +0900

    :sparkles: BJ 1929

--- src/BackJoon/실버/S3/BJ_1929.java
+++ src/BackJoon/실버/S3/BJ_1929.java
@@ -1,26 +1,42 @@
 package BackJoon.실버.S3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BJ_1929 {
+
+    public static boolean [] prime;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        boolean[] prime = new boolean[M+1];
+        prime = new boolean[N+1];
+        get_prime();
 
-        for (int i = 2; i <= M; i++) {
-            if(prime[i]) continue;
+        for (int i = M; i <= N; i++) {
+            if(!prime[i]) {
+                bw.write(i+"\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
 
-            if(i >= N){
+    public static void get_prime(){
+        prime[0] = prime[1] = true;
 
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) continue;
+            for (int j = i * i; j < prime.length; j+=i) {
+                prime[j] = true;
             }
         }
     }
-
 }
