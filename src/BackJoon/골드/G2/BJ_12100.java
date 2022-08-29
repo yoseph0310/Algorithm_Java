c01e8130c6b3aa58809f3dd9b4c771727b11e259
yoseph0310 <tjdytpq0310@gmail.com>
2022-08-29 13:12:04 +0900

    :modified: BJ_112100

--- src/BackJoon/골드/G2/BJ_12100.java
+++ src/BackJoon/골드/G2/BJ_12100.java
@@ -2,14 +2,6 @@ package BackJoon.골드.G2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-<<<<<<< HEAD
-
-public class BJ_12100 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    }
-}
-=======
 import java.util.StringTokenizer;
 
 public class BJ_12100 {
