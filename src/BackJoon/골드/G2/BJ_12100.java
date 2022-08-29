c6c8d0963d0ac6300a78a4067c40866449503de3
yoseph0310 <tjdytpq0310@gmail.com>
2022-08-29 12:26:42 +0900

    :recycle:

---
+++ src/BackJoon/골드/G2/BJ_12100.java
@@ -0,0 +1,10 @@
+package BackJoon.골드.G2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_12100 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
