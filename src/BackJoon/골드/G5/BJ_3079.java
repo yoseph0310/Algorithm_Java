da21884b993d7232770f7844bf258b6a25ab94f9
yoseph0310 <tjdytpq0310@gmail.com>
2022-09-28 10:04:24 +0900

    :sparkles: BJ 3079 G5

---
+++ src/BackJoon/골드/G5/BJ_3079.java
@@ -0,0 +1,55 @@
+package BackJoon.골드.G5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_3079 {
+
+    static int N, M;
+    static int [] times;
+    static long max = 0;
+    static long min = Long.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        times = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, times[i]);
+        }
+        getMinTime(N, M);
+        System.out.println(min);
+    }
+    public static void getMinTime(int n, int m){
+        long low = 0;
+        long high = max * m;
+
+        while (low <= high){
+            long mid = (low + high) / 2;
+            long sum = 0;
+
+            for (int time: times) {
+                long needPeople = mid / time;
+
+                if (sum >= m) break;
+                sum += needPeople;
+            }
+
+            if (sum >= m) {
+                System.out.println(mid + ", "+ sum);
+                min = Math.min(min, mid);
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+    }
+}
+// 2 3 3 4 6 8 9
