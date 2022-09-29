70603b3bb687d20d4cb7f37cfd05f82bfd1b5710
yoseph0310 <tjdytpq0310@gmail.com>
2022-09-29 20:33:37 +0900

    :sparkles: BJ 14501 퇴사 S3

---
+++ src/BackJoon/실버/S3/BJ_14501_퇴사.java
@@ -0,0 +1,33 @@
+package BackJoon.실버.S3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_14501_퇴사 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] T = new int[N+15];
+        int[] P = new int[N+15];
+        int[] DP = new int[N+15];
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i <= N; i++) {
+            DP[i] = Math.max(DP[i], max);
+
+            DP[T[i] + i] = Math.max(DP[T[i] + i], P[i] + DP[i]);
+
+            max = Math.max(max, DP[i]);
+        }
+        System.out.println(max);
+    }
+}
