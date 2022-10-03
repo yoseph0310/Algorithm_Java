5c2027da9cdba24198e915a216b94d82808c2ce4
서요셉 <tjdytpq0310@gmail.com>
2022-10-03 22:33:16 +0900

    :sparkles: BJ 15649 N과M(1) S3

---
+++ src/BackJoon/실버/S3/BJ_15649_N과M_1.java
@@ -0,0 +1,43 @@
+package BackJoon.실버.S3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_15649_N과M_1 {
+
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visited = new boolean[N];
+
+        dfs(N, M, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int n, int m, int depth) {
+        if (depth == m){
+            for (int value: arr) {
+                sb.append(value).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]){
+                visited[i] = true;
+                arr[depth] = i + 1;
+                dfs(n, m, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
