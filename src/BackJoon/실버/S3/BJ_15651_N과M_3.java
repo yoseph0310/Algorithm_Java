1497e21903b38fe5625bdc9a8f6cb237e23ecb3b
yoseph0310 <tjdytpq0310@gmail.com>
2022-10-04 09:45:24 +0900

    :sparkles: BJ 15651 N과M 3 S3

---
+++ src/BackJoon/실버/S3/BJ_15651_N과M_3.java
@@ -0,0 +1,38 @@
+package BackJoon.실버.S3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_15651_N과M_3 {
+
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth){
+        if (depth == M){
+            for (int val: arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth+1);
+        }
+    }
+}
