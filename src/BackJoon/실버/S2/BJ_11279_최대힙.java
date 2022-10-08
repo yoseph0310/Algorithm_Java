e21516a8a162864a07b625da975f25bfa66e6d73
서요셉 <tjdytpq0310@gmail.com>
2022-10-08 17:59:44 +0900

    :sparkles: BJ 11279 최대힙 S2

---
+++ src/BackJoon/실버/S2/BJ_11279_최대힙.java
@@ -0,0 +1,26 @@
+package BackJoon.실버.S2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class BJ_11279_최대힙 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0){
+                sb.append(pq.size() == 0 ? 0 : pq.poll()).append('\n');
+            } else {
+                pq.add(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
