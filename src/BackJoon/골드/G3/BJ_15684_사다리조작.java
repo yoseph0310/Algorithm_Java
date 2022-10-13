a85eb6448620aed5e0535ce72119e10d23aeedab
서요셉 <tjdytpq0310@gmail.com>
2022-10-14 00:27:08 +0900

    :sparkles: BJ 15684 사다리조작 G3

--- src/BackJoon/골드/G3/BJ_15684_사다리조작.java
+++ src/BackJoon/골드/G3/BJ_15684_사다리조작.java
@@ -8,7 +8,7 @@ public class BJ_15684_사다리조작 {
 
     static int N, M, H, ans;
     static int[][] board;
-    static boolean finish = false;
+    static boolean isFinish = false;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,36 +19,61 @@ public class BJ_15684_사다리조작 {
         H = Integer.parseInt(st.nextToken());
         board = new int[H+1][N+1];
 
-        int x, y;
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             board[x][y] = 1;
             board[x][y+1] = 2;
         }
 
         for (int i = 0; i <= 3; i++) {
             ans = i;
-            dfs(1, 0);
-            if (finish) break;
+            dfs(1, 1, 0);
+            if (isFinish) break;
         }
 
-        System.out.println((finish) ? ans : -1);
-    }
-    static void dfs(int x, int cnt){
-
+        System.out.println((isFinish) ? ans : -1);
     }
 
-    static boolean check(){
-        for (int i = 1; i <= N; i++) {
-            int x = 1, y = i;
-            for (int j = 0; j < H; j++) {
-                if (board[x][y] == 1) y++;
-                else if (board[x][y] == 2) y--;
-                x++;
+    static void dfs(int x, int y, int garoCnt) {
+        if (isFinish) return;
+        if (ans == garoCnt) {
+            if (isSuccess()) {
+                isFinish = true;
             }
-            if (y != i) return false;
+            return;
+        }
+
+        for (int i = x; i <= H; i++) {
+            for (int j = y; j < N; j++) {
+                if (board[i][j] == 0 && board[i][j + 1] == 0){
+                    board[i][j] = 1;
+                    board[i][j+1] = 2;
+
+                    dfs(1, 1, garoCnt + 1);
+
+                    board[i][j] = 0;
+                    board[i][j + 1] = 0;
+                }
+            }
+        }
+    }
+
+    static boolean isSuccess(){
+        for (int i = 1; i <= N; i++) {
+            int nx = 1;
+            int ny = i;
+
+            while (nx <= H){
+                if (board[nx][ny] == 1) ny++;
+                else if (board[nx][ny] == 2) ny--;
+                nx++;
+            }
+
+            if (ny != i) return false;
         }
         return true;
     }
