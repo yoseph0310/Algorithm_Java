9c696c831d05dcf816e42550a4dd1f7e77e834e2
서요셉 <tjdytpq0310@gmail.com>
2022-11-06 23:33:21 +0900

    :sparkles: BJ 2638 치즈 G3

--- src/BackJoon/골드/G3/BJ_2638_치즈.java
+++ src/BackJoon/골드/G3/BJ_2638_치즈.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class BJ_2638_치즈 {
 
-    static int N, M, cheeseCnt, ans;
+    static int N, M, ans;
     static int[][] board;
     static ArrayList<Point> cheeseList;
 
@@ -27,54 +27,26 @@ public class BJ_2638_치즈 {
             for (int j = 0; j < M; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
                 if (board[i][j] == 1) {
-                    cheeseCnt++;
                     cheeseList.add(new Point(i, j));
                 }
             }
         }
 
         progress();
+
     }
 
     static void progress() {
         while (cheeseList.size() != 0) {
             ans++;
             bfs();
-            meltCheese();
+            meltingCheese();
         }
         System.out.println(ans);
     }
 
-    static void bfs(){
-        Queue<Point> q = new LinkedList<>();
-        boolean[][] visited = new boolean[N][M];
-        q.add(new Point(0, 0));
-        visited[0][0] = true;
-        board[0][0] = 2;
-
-        while(!q.isEmpty()) {
-            Point cur = q.poll();
-
-            int cx = cur.x;
-            int cy = cur.y;
-
-            for (int d = 0; d < 4; d++) {
-                int nx = cx + dx[d];
-                int ny = cy + dy[d];
-
-                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                    if (!visited[nx][ny] && board[nx][ny] != 1) {
-                        board[nx][ny] = 2;
-                        q.add(new Point(nx, ny));
-                        visited[nx][ny] = true;
-                    }
-                }
-            }
-        }
-    }
-
-    static void meltCheese() {
-        for (Iterator<Point> it = cheeseList.iterator(); it.hasNext();) {
+    static void meltingCheese() {
+        for (Iterator<Point> it = cheeseList.iterator(); it.hasNext(); ) {
             Point p = it.next();
             int x = p.x;
             int y = p.y;
@@ -91,26 +63,48 @@ public class BJ_2638_치즈 {
 
             if (cnt >= 2) {
                 board[x][y] = 0;
-                cheeseCnt--;
                 it.remove();
             }
-
         }
     }
 
-    static boolean isAllMelt(int[][] board) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (board[i][j] == 1) return false;
+    static void bfs() {
+        Queue<Point> q = new LinkedList<>();
+        boolean[][] visited = new boolean[N][M];
+
+        q.add(new Point(0, 0));
+        visited[0][0] = true;
+        board[0][0] = 2;
+
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+
+            int cx = cur.x;
+            int cy = cur.y;
+
+            for (int d = 0; d < 4; d++) {
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+
+                if (isBoundary(nx, ny)) {
+                    if (!visited[nx][ny] && board[nx][ny] != 1) {
+                        board[nx][ny] = 2;
+                        q.add(new Point(nx, ny));
+                        visited[nx][ny] = true;
+                    }
+                }
             }
         }
-        return true;
+    }
+
+    static boolean isBoundary(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < M;
     }
 
     static class Point {
         int x, y;
 
-        public Point(int x, int y){
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
