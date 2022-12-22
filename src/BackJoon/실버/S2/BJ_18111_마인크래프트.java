f3552aa3cd009526779d57e75d5199f3aa251723
서요셉 <tjdytpq0310@gmail.com>
2022-12-23 00:14:12 +0900

    :sparkles: BJ 18111 마인크래프트 S2

--- src/BackJoon/실버/S2/BJ_18111_마인크래프트.java
+++ src/BackJoon/실버/S2/BJ_18111_마인크래프트.java
@@ -6,19 +6,82 @@ import java.util.StringTokenizer;
 
 public class BJ_18111_마인크래프트 {
 
-    static int N, M, B;
     static int[][] board;
+    static int[] result = new int[2];
+
+    static int time = Integer.MAX_VALUE;
+    static int height;
+    static int block;
+    static int N, M, B;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
 
+        board = new int[N][M];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, board[i][j]);
+            }
+        }
 
+        brute_force(max);
+
+        System.out.println(time + " " + height);
+    }
+
+    static void brute_force(int max) {
+        for (int i = 0; i <= max; i++) {
+            result = excavation(i);
+
+            if (time > result[0]) {
+                time = result[0];
+                height = result[1];
+            }
+            else if (time == result[0] && height < result[1]) {
+                time = result[0];
+                height = result[1];
+            }
+        }
+    }
+
+    static int[] excavation(int height) {
+        int block = B;
+        int time = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                int value = board[i][j];
+
+                if (value == height) {
+                    continue;
+                }
+
+                if (value > height) {
+                    time += (value - height) * 2;
+                    block += (value - height);
+                } else {
+                    time += height - value;
+                    block -= (height - value);
+                }
+            }
+        }
+        if (block < 0) {
+            result[0] = 999999999;
+            return result;
+        }
+
+        result[0] = time;
+        result[1] = height;
+
+        return result;
     }
 }
-
-/*
-    1. 주어진 보드에서 높이가 다른데 그 수가 가장 많은 부분을 제외하고 그 좌표를 저장
-    2. 높이가 다른 곳과 평평한 곳과의 높이를 B와 비교해서
- */
