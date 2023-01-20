11f9ca6d28daad7a1a077072b16e41744d2ffd4e
서요셉 <tjdytpq0310@gmail.com>
2023-01-20 16:58:12 +0900

    :sparkles: BJ 11660 구간 합 구하기 5 S1

---
+++ src/BackJoon/실버/S1/BJ_11660_구간_합_구하기_5.java
@@ -0,0 +1,45 @@
+package BackJoon.실버.S1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_11660_구간_합_구하기_5 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] board = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] D = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + board[i][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            int sum = 0;
+
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            sum = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+
+            System.out.println(sum);
+        }
+
+    }
+}
