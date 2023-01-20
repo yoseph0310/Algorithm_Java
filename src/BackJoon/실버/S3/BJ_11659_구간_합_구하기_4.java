0d7822e420e3ca5347bacb5fedb4ac171485d286
서요셉 <tjdytpq0310@gmail.com>
2023-01-20 16:27:53 +0900

    :sparkles: BJ 11659 구간 합 구하기 4 S3
    BJ 11659 구간 합 구하기 4 S3

---
+++ src/BackJoon/실버/S3/BJ_11659_구간_합_구하기_4.java
@@ -0,0 +1,30 @@
+package BackJoon.실버.S3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_11659_구간_합_구하기_4 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] S = new long[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println(S[b] - S[a - 1]);
+        }
+
+    }
+}
