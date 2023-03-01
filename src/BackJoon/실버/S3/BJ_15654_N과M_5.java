0014e46dd7f8590de380ea92636f2da3a398b70f
서요셉 <tjdytpq0310@gmail.com>
2023-03-01 18:45:25 +0900

    :sparkles: BJ 15654 N과M (5)

---
+++ src/BackJoon/실버/S3/BJ_15654_N과M_5.java
@@ -0,0 +1,54 @@
+package BackJoon.실버.S3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_15654_N과M_5 {
+
+    static int N, M;
+    static int[] arr;
+    static int[] selected;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        selected = new int[M];
+        visited = new boolean[10001];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int v : selected) {
+                sb.append(v).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[arr[i]]) {
+                visited[arr[i]] = true;
+                selected[depth] = arr[i];
+                dfs(depth + 1);
+                visited[arr[i]] = false;
+            }
+        }
+    }
+}
