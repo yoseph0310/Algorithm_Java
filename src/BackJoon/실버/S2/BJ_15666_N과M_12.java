d81e28b0fed675874244c41b9f6fdffb539f2c08
서요셉 <tjdytpq0310@gmail.com>
2023-03-01 19:36:05 +0900

    :sparkles: BJ 15666 N과M (12)

---
+++ src/BackJoon/실버/S2/BJ_15666_N과M_12.java
@@ -0,0 +1,52 @@
+package BackJoon.실버.S2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_15666_N과M_12 {
+
+    static int N, M;
+    static int[] arr, selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        selected = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        comb(0, 0);
+        System.out.println(sb);
+    }
+
+    static void comb(int start, int depth) {
+        if (depth == M) {
+            for (int v: selected) {
+                sb.append(v).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int past = -1;
+        for (int i = start; i < N; i++) {
+            int now = arr[i];
+            if (past != now) {
+                past = now;
+                selected[depth] = arr[i];
+                comb(i, depth + 1);
+            }
+        }
+    }
+}
