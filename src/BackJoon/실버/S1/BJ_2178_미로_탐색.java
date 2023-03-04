fa77c9d625f562b38f125187e37adcbda9751da0
서요셉 <tjdytpq0310@gmail.com>
2023-03-04 18:38:08 +0900

    :sparkles: BJ 2178 미로탐색

---
+++ src/BackJoon/실버/S1/BJ_2178_미로_탐색.java
@@ -0,0 +1,82 @@
+package BackJoon.실버.S1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_2178_미로_탐색 {
+
+    static int N, M;
+    static int min = Integer.MAX_VALUE;
+    static int[][] board;
+    static boolean[][] visited;
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        bfs();
+        System.out.println(min);
+    }
+
+    static void bfs() {
+        Queue<Point> q = new LinkedList<>();
+
+        q.add(new Point(0, 0, 1));
+        visited[0][0] = true;
+
+        while(!q.isEmpty()) {
+            Point cur = q.poll();
+
+            int cx = cur.x;
+            int cy = cur.y;
+
+            if (cx == N - 1 && cy == M - 1) {
+                min = Math.min(min, cur.cnt);
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+
+                if (isBoundary(nx, ny)) {
+                    if (!visited[nx][ny] && board[nx][ny] == 1) {
+                        q.add(new Point(nx, ny, cur.cnt + 1));
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    static boolean isBoundary(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < M;
+    }
+
+    static class Point {
+        int x, y, cnt;
+
+        public Point(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+}
