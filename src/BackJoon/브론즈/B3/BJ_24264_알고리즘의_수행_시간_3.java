9957a06719928de25a7e6f2f07f728a9ce671347
서요셉 <tjdytpq0310@gmail.com>
2023-03-21 16:24:24 +0900

    :sparkles: BJ 24264 알고리즘의 수행 시간 3

---
+++ src/BackJoon/브론즈/B3/BJ_24264_알고리즘의_수행_시간_3.java
@@ -0,0 +1,15 @@
+package BackJoon.브론즈.B3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_24264_알고리즘의_수행_시간_3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Long N = Long.parseLong(br.readLine());
+
+        System.out.println(N * N);
+        System.out.println(2);
+    }
+}
