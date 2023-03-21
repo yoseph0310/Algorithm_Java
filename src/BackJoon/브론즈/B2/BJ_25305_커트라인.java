d731abf9a6a7fffe431ce9fc0ed96cabb229a419
서요셉 <tjdytpq0310@gmail.com>
2023-03-21 17:33:17 +0900

    :sparkles: BJ 25305 커트라인

---
+++ src/BackJoon/브론즈/B2/BJ_25305_커트라인.java
@@ -0,0 +1,27 @@
+package BackJoon.브론즈.B2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BJ_25305_커트라인 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Integer [] arr = new Integer[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        System.out.println(arr[K - 1]);
+    }
+}
