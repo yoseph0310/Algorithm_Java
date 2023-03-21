bc866b95357a0d7c21056b2b9d9c2b735b55ff8f
서요셉 <tjdytpq0310@gmail.com>
2023-03-21 16:20:57 +0900

    :sparkles: BJ 24263 알고리즘의 수행 시간 2

---
+++ src/BackJoon/브론즈/B4/BJ_24263_알고리즘의_수행_시간_2.java
@@ -0,0 +1,15 @@
+package BackJoon.브론즈.B4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_24263_알고리즘의_수행_시간_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N);
+        System.out.println(1);
+    }
+}
