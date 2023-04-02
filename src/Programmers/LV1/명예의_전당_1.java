84ed14edb9867285aad3240d182e6cdd65dcf462
서요셉 <tjdytpq0310@gmail.com>
2023-04-02 17:54:14 +0900

    :sparkles: 명예의 전당 (1) LV1

---
+++ src/Programmers/LV1/명예의_전당_1.java
@@ -0,0 +1,22 @@
+package Programmers.LV1;
+
+public class 명예의_전당_1 {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            if (i < k - 1) {
+                list.add(score[i]);
+                list.sort(Collections.reverseOrder());
+                answer[i] = list.get(list.size() - 1);
+            } else {
+                list.add(score[i]);
+                list.sort(Collections.reverseOrder());
+                answer[i] = list.get(k - 1);
+            }
+        }
+
+        return answer;
+    }
+}
