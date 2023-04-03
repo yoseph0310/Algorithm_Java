74aeed10b8416af08fedd503e6ce6db0add5464c
서요셉 <tjdytpq0310@gmail.com>
2023-04-03 17:55:57 +0900

    :sparkles: 가장 가까운 같은 글자 LV1

---
+++ src/Programmers/LV1/가장_가까운_같은_글자.java
@@ -0,0 +1,25 @@
+package Programmers.LV1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 가장_가까운_같은_글자 {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (map.containsKey(c)) {
+                answer[i] = i - map.get(c);
+            } else {
+                answer[i] = -1;
+            }
+            map.put(c, i);
+        }
+
+        return answer;
+    }
+}
