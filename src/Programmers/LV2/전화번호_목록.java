899ca586baf716035a32383fae89d40e3ae8fc09
서요셉 <tjdytpq0310@gmail.com>
2023-04-24 23:27:18 +0900

    :sparkles: 전화번호 목록 LV2

---
+++ src/Programmers/LV2/전화번호_목록.java
@@ -0,0 +1,25 @@
+package Programmers.LV2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 전화번호_목록 {
+    public boolean solution(String[] phone_book) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], i);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (map.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
