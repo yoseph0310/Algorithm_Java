945167c91de1b73499eb539551cfe02717a376b9
서요셉 <tjdytpq0310@gmail.com>
2023-04-25 18:46:22 +0900

    :sparkles: 트럭 LV2

---
+++ src/Programmers/LV2/트럭.java
@@ -0,0 +1,36 @@
+package Programmers.LV2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 트럭 {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+
+        int max = 0;
+        for( int w : truck_weights){
+            while(true){
+                if (q.isEmpty()){
+                    q.offer(w);
+                    max += w;
+                    answer++;
+                    break;
+                } else if (q.size() == bridge_length) {
+                    max -= q.poll();
+                } else {
+                    if (max + w > weight){
+                        q.offer(0);
+                        answer++;
+                    } else {
+                        q.offer(w);
+                        max += w;
+                        answer++;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer + bridge_length;
+    }
+}
