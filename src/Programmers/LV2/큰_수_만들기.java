3562f063987b177862055308c0cb0277bda161c5
서요셉 <tjdytpq0310@gmail.com>
2023-04-29 06:22:42 +0900

    :sparkles: 컬러링 북 LV2

--- src/Programmers/LV2/큰_수_만들기.java
+++ src/Programmers/LV2/큰_수_만들기.java
@@ -21,4 +21,24 @@ public class 큰_수_만들기 {
 
         return answer.toString();
     }
+
+    public String solution2(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+
+        int len = number.length() - k;
+        int start = 0;
+
+        while (start < number.length() && answer.length() != len) {
+            int leftNum = k + answer.length() + 1;
+            int max = 0;
+            for (int j = start; j < leftNum; j++) {
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    start = j + 1;
+                }
+            }
+            answer.append(Integer.toString(max));
+        }
+        return answer.toString();
+    }
 }
