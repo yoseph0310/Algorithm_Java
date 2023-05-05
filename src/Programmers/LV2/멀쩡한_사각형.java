a3192629b6ca10bfafce5103588016b148031a14
서요셉 <tjdytpq0310@gmail.com>
2023-05-05 18:25:49 +0900

    :sparkles: 멀쩡한 사각형 LV2

---
+++ src/Programmers/LV2/멀쩡한_사각형.java
@@ -0,0 +1,19 @@
+package Programmers.LV2;
+
+public class 멀쩡한_사각형 {
+    public long solution(int w, int h) {
+        long answer = 1;
+        long width = w;
+        long height = h;
+
+        answer = (w * h) - (w + h - gcd(w,h));
+
+        return answer;
+    }
+
+    long gcd(long w, long h) {
+        if (h == 0) return w;
+        return gcd(h, w % h);
+    }
+}
+}
