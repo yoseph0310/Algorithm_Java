4d08cfa07aebc972c0bdf617b530b5f6dd24aaea
서요셉 <tjdytpq0310@gmail.com>
2023-05-08 17:27:23 +0900

    :sparkles: BJ 9663 NQueen G4

---
+++ src/BackJoon/골드/G4/BJ_9663_N_Queen.java
@@ -0,0 +1,47 @@
+package BackJoon.골드.G4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_9663_N_Queen {
+
+    static int N;
+    static int answer = 0;
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        nQueen(0);
+    }
+
+    static void nQueen(int depth) {
+        if (depth == N) {
+            answer++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i;
+            if (isPossible(depth)) {
+                nQueen(depth + 1);
+            }
+        }
+    }
+
+    static boolean isPossible(int col) {
+        for (int i = 0; i < col; i++) {
+            if (arr[col] == arr[i]) {
+                return false;
+            }
+            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
