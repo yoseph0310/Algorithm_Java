a958e99a79d05f04bd1240684502d5da21eba8a9
서요셉 <tjdytpq0310@gmail.com>
2023-05-31 17:13:25 +0900

    :sparkles: 디펜스 게임 LV2

---
+++ src/Programmers/LV2/디펜스_게임.java
@@ -0,0 +1,26 @@
+package Programmers.LV2;
+import java.util.*;
+
+public class 디펜스_게임 {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = enemy.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < enemy.length; i++) {
+            n -= enemy[i];
+            pq.add(enemy[i]);
+
+            if (n < 0) {
+                if (k > 0 && !pq.isEmpty()) {
+                    k--;
+                    n += pq.poll();
+                } else {
+                    answer = i;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
