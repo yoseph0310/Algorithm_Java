f6670409219c5344ed05e600bee486862bbbb2b9
서요셉 <tjdytpq0310@gmail.com>
2023-08-09 23:08:17 +0900

    :sparkles: CodeBattle [No_2] 삼국지 게임 Pro

--- src/SWEA/CodeBattle/실전실습/No_2_삼국지게임/UserSolution.java
+++ src/SWEA/CodeBattle/실전실습/No_2_삼국지게임/UserSolution.java
@@ -6,24 +6,23 @@ import java.util.List;
 
 public class UserSolution {
 
-    static int[] parent;                // 노드의 부모 정보    -> 25 * 25 = 625, attack 으로 인한 새 국가 생성 가능성 최대 8,000 : 8,625
-    static List<Integer>[] unionList;            // 각 군주들의 동맹 리스트    -> 625
-    static List<Integer>[] enemyList;            // 각 군주들의 적군 리스트    -> 625
-
-    // 각 국가의 정보
     static class NationInfo {
         int x, y, soldierNum;
     }
 
-    static NationInfo[] nationInfoList;        // 노드별 국가 정보    -> 8625
-    static int[][] nationPosition;             // 각 국가의 좌표     -> 25 * 25
+    static int[] parents;
+    static List<Integer>[] unionList;
+    static List<Integer>[] enemyList;
 
-    static HashMap<String, Integer> monarchHM;  // 군주의 이름을 좌표로 표현하기 위한 해시맵  key : string(군주 이름을 string 으로 변환), value : integer (idx)
-    static int nationCnt;                      // id 설정을 위한 국가 개수
+    static int[][] nationPosition;
+    static NationInfo[] nationInfoList;
+
+    static HashMap<String, Integer> monarchHM;
+    static int nationCnt;
 
     static int N;
 
-    // 8방 확인 - 상, 상우, 우, 우하, 하, 좌하, 좌, 좌상
+    // 상 우상 우 우하 하 좌하 좌 좌상
     static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
 
@@ -38,9 +37,8 @@ public class UserSolution {
      */
     void init(int n, int[][] mSoldier, char[][][] mMonarch) {
         N = n;
-        int nationID = 0;
 
-        parent = new int[8625];
+        parents = new int[8625];
         unionList = new ArrayList[625];
         enemyList = new ArrayList[625];
         for (int i = 0; i < 625; i++) {
@@ -48,36 +46,37 @@ public class UserSolution {
             enemyList[i] = new ArrayList<>();
         }
 
-        nationInfoList = new NationInfo[8625];
         nationPosition = new int[25][25];
+        nationInfoList = new NationInfo[8625];
         for (int i = 0; i < 8625; i++) {
             nationInfoList[i] = new NationInfo();
         }
 
-        monarchHM = new HashMap<String, Integer>();
+        monarchHM = new HashMap<>();
         nationCnt = 0;
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                String mMonarchName = charToString(mMonarch[i][j]);
+                String monarchName = charToString(mMonarch[i][j]);
 
-                // string -> id 정보를 해시맵에 저장, 각 국가의 id 를 2차원 배열에 마킹
-                monarchHM.put(mMonarchName, monarchHM.getOrDefault(mMonarchName, 0) + nationID);
-                nationPosition[i][j] = nationID;
+                // String -> int idx 로 변환 : HM 사용
+                monarchHM.put(monarchName, monarchHM.getOrDefault(monarchName, 0) + nationCnt);
 
-                // 국가 정보 입력
-                nationInfoList[nationID].soldierNum = mSoldier[i][j];
-                nationInfoList[nationID].x = i;
-                nationInfoList[nationID].y = j;
+                // 최초 부모는 자기 자신
+                parents[nationCnt] = nationCnt;
 
-                // 최초 부모 노드는 자기 자신
-                parent[nationID] = nationID;
+                // 위치 기록
+                nationPosition[i][j] = nationCnt;
 
-                // 동맹 리스트에 자기 자신 추가
-                unionList[nationID].add(nationID);
+                // 국가 정보 기록
+                nationInfoList[nationCnt].soldierNum = mSoldier[i][j];
+                nationInfoList[nationCnt].x = i;
+                nationInfoList[nationCnt].y = j;
 
-                // 다음 국가 ID를 위해 nationID 증가
-                nationID++;
+                // 자기 자신도 자신의 동맹에 추가
+                unionList[nationCnt].add(nationCnt);
+
+                nationCnt++;
             }
         }
 
@@ -112,12 +111,13 @@ public class UserSolution {
         int idxA = monarchHM.get(monarchA);
         int idxB = monarchHM.get(monarchB);
 
-        // A, B 가 이미 동맹이면 -1
-        if (isUnion(idxA, idxB)) return -1;
-        // A, B 동맹들 사이에 적대관계가 있으면 -2
-        if (isEnemy(idxA, idxB)) return -2;
+        if (isUnion(idxA, idxB)) {
+            return -1;
+        }
+        if (isEnemy(idxA, idxB)) {
+            return -2;
+        }
 
-        // 위 경우 아니라면 union
         mkUnion(idxA, idxB);
 
         return 1;
@@ -166,10 +166,12 @@ public class UserSolution {
         int idxA = monarchHM.get(monarchA);
         int idxB = monarchHM.get(monarchB);
 
-        // 같은 동맹인지 확인
-        if (isUnion(idxA, idxB)) return -1;
+        // 동맹 관계면 -1 반환
+        if (isUnion(idxA, idxB)) {
+            return -1;
+        }
 
-        // A 동맹, B 동맹 들이 B 영토에 인접하지 않으면 전투가 일어나지 않으므로 인접하는지 확인
+        // 인접 동맹 확인
         int Bx = nationInfoList[idxB].x;
         int By = nationInfoList[idxB].y;
 
@@ -178,8 +180,8 @@ public class UserSolution {
             int nx = Bx + dx[d];
             int ny = By + dy[d];
 
-            // 경계선 안이고 다음 좌표에 A의 동맹군이 하나라도 있다면 전투는 시작 된다.
             if (isBoundary(nx, ny)) {
+                // 적군의 루트와 다음 좌표에 위치한 군주의 루트가 같으면 적군이 존재하는 것
                 if (find(idxA) == find(nationPosition[nx][ny])) {
                     isEnemy = true;
                     break;
@@ -187,27 +189,29 @@ public class UserSolution {
             }
         }
 
-        if (!isEnemy) return -2;
+        // 하나라도 적군이 없다면 -2 반환
+        if (!isEnemy) {
+            return -2;
+        }
 
-        // 전투 시작
-        mkEnemy(find(idxA), find(idxB));
+        mkEnemy(idxA, idxB);
 
         int totalSoldierA = 0;
-        int totalSoldierB = nationInfoList[nationPosition[Bx][By]].soldierNum;
-        int dispatchCnt;
+        int totalSoldierB = nationInfoList[idxB].soldierNum;
+        int dispatchCnt = 0;
 
         for (int d = 0; d < 8; d++) {
             int nx = Bx + dx[d];
             int ny = By + dy[d];
 
             if (isBoundary(nx, ny)) {
-                // 적군이면
+                // B 기준 - A는 적군. 적군이면 병사 수 / 2 파견
                 if (find(idxA) == find(nationPosition[nx][ny])) {
                     dispatchCnt = nationInfoList[nationPosition[nx][ny]].soldierNum / 2;
                     nationInfoList[nationPosition[nx][ny]].soldierNum -= dispatchCnt;
                     totalSoldierA += dispatchCnt;
                 }
-                // 아군이면
+                // B 기준 - B 아군
                 else if (find(idxB) == find(nationPosition[nx][ny])) {
                     dispatchCnt = nationInfoList[nationPosition[nx][ny]].soldierNum / 2;
                     nationInfoList[nationPosition[nx][ny]].soldierNum -= dispatchCnt;
@@ -216,26 +220,27 @@ public class UserSolution {
             }
         }
 
-        // 방어 성공
+        // 방어 성공 0 리턴
         if (totalSoldierB >= totalSoldierA) {
             nationInfoList[idxB].soldierNum = totalSoldierB - totalSoldierA;
             return 0;
         }
 
-        // 방어 실패 - 공격 대상지는 삭제된다.
-        unionList[find(idxB)].remove((Integer) idxB); // idxB 를 인덱스로 인식하고 있다.
+        // 공격 성공하면 공격지 군주는 처형
+        unionList[find(idxB)].remove((Integer) idxB);
 
-        // 해당 영토의 병사 수는 A - B. 동맹관계, 적대관계도 없는 새로운 영토가 된다.
-        nationInfoList[idxB].soldierNum = totalSoldierA - totalSoldierB;
+        // 공격지였던 영토는 동맹관계, 적대관계도 없는 새 영토가 된다.
+        nationInfoList[idxB].soldierNum = totalSoldierA - totalSoldierB;        // 남은 병사 결정
 
-        nationInfoList[nationCnt] = nationInfoList[idxB];       // 새로운 id 에 idxB 의 위치를 대입
-        nationPosition[nationInfoList[nationCnt].x][nationInfoList[nationCnt].y] = nationCnt;   // 해당 좌표에 새로운 id 부여
+        // 새 군주 ID 인 nationCnt 로 정보들을 새로 입력
+        nationInfoList[nationCnt] = nationInfoList[idxB];
+        nationPosition[nationInfoList[nationCnt].x][nationInfoList[nationCnt].y] = nationCnt;
 
-        // mGeneral 이 새 영토의 주인이 된다.
+        // 새로운 군주 등극
         monarchHM.put(general, monarchHM.getOrDefault(general, 0) + nationCnt);
 
-        // 동맹 관계 편입
-        parent[nationCnt] = find(idxA);
+        // 동맹은 A 에 편입, 적대관계 또한 동일한다.
+        parents[nationCnt] = find(idxA);
         unionList[find(idxA)].add(nationCnt);
 
         nationCnt++;
@@ -272,14 +277,11 @@ public class UserSolution {
 
         int recruitCnt = 0;
 
-        // monarch 만 mNum 병사 모집
         if (mOption == 0) {
-            int idxA = monarchHM.get(monarch);
-            nationInfoList[idxA].soldierNum += mNum;
-            recruitCnt = nationInfoList[idxA].soldierNum;
-        }
-        // monarch 포함 모든 동맹 mNum 병사 모집
-        else {
+            int idx = monarchHM.get(monarch);
+            nationInfoList[idx].soldierNum += mNum;
+            recruitCnt = nationInfoList[idx].soldierNum;
+        } else {
             int rootA = find(monarchHM.get(monarch));
             for (int allies: unionList[rootA]) {
                 nationInfoList[allies].soldierNum += mNum;
@@ -290,34 +292,30 @@ public class UserSolution {
         return recruitCnt;
     }
 
-    // x 의 부모를 찾는다
     static int find(int x) {
-        if (x == parent[x]) return x;
-        else return parent[x] = find(parent[x]);
+        if (x == parents[x]) return x;
+        else return parents[x] = find(parents[x]);
     }
 
-    // x, y 의 부모가 다르면 조합한다.
-    // 부모 쪽 기준으로 두고 부모를 x라 하면
-    // x 동맹 리스트에 y 의 동맹리스트 추가
-    // x 적군 리스트에 y 의 적군리스트 추가
     static void mkUnion(int x, int y) {
         x = find(x);
         y = find(y);
 
+        // x 기준으로 몰아준다.
         if (x != y) {
-            parent[y] = x;
+            parents[y] = x;
 
-            // y 의 동맹리스트, 적군 리스트 x 에도 추가
-            for (int n: unionList[y]) {
-                unionList[x].add(n);
+            // x 의 동맹 리스트에 y 동맹 리스트 추가
+            for (int allies: unionList[y]) {
+                unionList[x].add(allies);
             }
-            for (int n: enemyList[y]) {
-                enemyList[x].add(n);
+            // x 의 적 리스트에 y 적 리스트 추가
+            for (int enemy : enemyList[y]) {
+                enemyList[x].add(enemy);
             }
         }
     }
 
-    // x, y 가 서로 같은 동맹인지 판단한다.
     static boolean isUnion(int x, int y) {
         x = find(x);
         y = find(y);
@@ -325,25 +323,23 @@ public class UserSolution {
         return x == y;
     }
 
-    // x, y 가 적군인지 판단한다.
     static boolean isEnemy(int x, int y) {
         x = find(x);
         y = find(y);
 
-        // x 의 적군리스트에서 y 가 있다면 true 반환
         for (int enemy : enemyList[x]) {
-            // 전부 다 돌지 않고 enemy 를 find() 넣고 돌려 y 가 나오면 적군임
-            if (find(enemy) == y) return true;
+            if (find(enemy) == y) {
+                return true;
+            }
         }
+
         return false;
     }
 
-    // x, y 를 서로의 적군으로 추가한다.
     static void mkEnemy(int x, int y) {
         x = find(x);
         y = find(y);
 
-        // 동맹이거나 이미 적군이면 리턴
         if (x == y || isEnemy(x, y)) return;
 
         enemyList[x].add(y);
