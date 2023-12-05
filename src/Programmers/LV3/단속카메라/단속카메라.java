6360a1a7b4190ca7e9e9d852fb79d8a9c22c937a
seoyoseb <tjdytpq0310@gmail.com>
2023-12-05 13:30:47 +0900

    :sparkles: 단속카메라 LV3

--- src/Programmers/LV3/단속카메라/단속카메라.java
+++ src/Programmers/LV3/단속카메라/단속카메라.java
@@ -1,4 +1,4 @@
-package Programmers.LV3;
+package Programmers.LV3.단속카메라;
 import java.util.*;
 
 public class 단속카메라 {
@@ -8,7 +8,7 @@ public class 단속카메라 {
 
             Arrays.sort(routes, (int[] o1, int[] o2) -> (o1[1] - o2[1]));
 
-            int cam = Integer.MIN_VALUE;
+            int cam = -30_001;
 
             for (int[] route : routes) {
                 if (cam < route[0]) {
