8da1ed0b9cf1078a78f34329f356be57e4c4df17
seoyoseb <tjdytpq0310@gmail.com>
2023-12-05 13:48:34 +0900

    :sparkles: 불량 사용자 LV3

--- src/Programmers/LV3/불량사용자/불량_사용자.java
+++ src/Programmers/LV3/불량사용자/불량_사용자.java
@@ -1,53 +1,55 @@
-package Programmers.LV3;
+package Programmers.LV3.불량사용자;
 import java.util.*;
 
+// 유저 아이디에서 밴아이디라고 생각되는 아이디를 추가하여 담을 셋.
+// 그 셋들을 모으는 셋.
+// dfs 에서 모든 밴 아이디를 체크했다면 끝
 public class 불량_사용자 {
     class Solution {
 
-        static String[] userIds;
-        static String[] bannedIds;
-        static HashSet<HashSet<String>> result = new HashSet<>();
+        String[] userIds, bannedIds;
+        HashSet<HashSet<String>> hs;
 
         public int solution(String[] user_id, String[] banned_id) {
             userIds = user_id;
             bannedIds = banned_id;
 
-            dfs(new HashSet<>(), 0);
+            hs = new HashSet<>();
 
-            return result.size();
+            DFS(new HashSet<>(), 0);
+
+            return hs.size();
         }
 
-        static void dfs(HashSet<String> set, int depth) {
+        void DFS(HashSet<String> set, int depth) {
             if (depth == bannedIds.length) {
-                result.add(set);
+                hs.add(set);
                 return;
             }
 
             for (int i = 0; i < userIds.length; i++) {
-                if (set.contains(userIds[i])) {
-                    continue;
-                }
+                if (set.contains(userIds[i])) continue;
 
                 if (check(userIds[i], bannedIds[depth])) {
                     set.add(userIds[i]);
-                    dfs(new HashSet<>(set), depth + 1);
+                    DFS(new HashSet<>(set), depth + 1);
                     set.remove(userIds[i]);
                 }
             }
         }
 
-        static boolean check(String userId, String bannedId) {
+        boolean check(String userId, String bannedId) {
             if (userId.length() != bannedId.length()) return false;
 
-            boolean match = true;
+            boolean isMatch = true;
             for (int i = 0; i < userId.length(); i++) {
                 if (bannedId.charAt(i) != '*' && userId.charAt(i) != bannedId.charAt(i)) {
-                    match = false;
+                    isMatch = false;
                     break;
                 }
             }
 
-            return match;
+            return isMatch;
         }
     }
 }
