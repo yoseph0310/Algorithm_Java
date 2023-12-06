826b5127152193f662223ded44b6f9bc741d2ad1
seoyoseb <tjdytpq0310@gmail.com>
2023-12-06 18:47:19 +0900

    :sparkles: 메이즈러너 LV3

--- src/CodeTree/삼성스터디/메이즈러너/메이즈러너.java
+++ src/CodeTree/삼성스터디/메이즈러너/메이즈러너.java
@@ -1,19 +1,45 @@
-package CodeTree.삼성스터디;
+package CodeTree.삼성스터디.메이즈러너;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 메이즈_러너 {
+/**
+ * - 개체
+ * 1. 미로
+ *  - N * N 크기 격자. 좌상단 (1, 1)
+ *      - 빈칸 : 참가자 이동 가능
+ *      - 벽 : 참가자 이동 불가, 1 ~ 9 이하 내구도. 내구도는 회전할 때 1감소, 내구도가 0이되면 빈칸이 된다. ( -1 ~ -9 로 변경)
+ *      - 출구 : 참가자가 도착하면 즉시 탈출 (-10으로 변경)
+ *
+ * 2. 참가자 : 모든 참가자는 1초마다 한 칸씩 움직인다.
+ *  - 두 위치의 최단거리는 맨하탄 거리로 측정
+ *  - 모든 참가자는 동시에 움직임
+ *  - 상화좌우 이동, 벽이 없는 곳으로 이동
+ *  - 움직이는 칸은 현재 머무르는 칸보다 출구와 가까워야 함
+ *  - 이동가능 칸이 2개 이상이면 상하가 우선순위
+ *  - 움직일 수 없는 상황이면 움직이지 않는다.
+ *  - 한 칸에 2명이 존재할 수 있음
+ *
+ *  - 진행
+ *      1. 참가자 이동
+ *      2. 미로 회전
+ *          - 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형을 잡는다.
+ *          - 가장 작은 크기를 갖는 정사각형이 2개 이상이면 좌상단 r 좌표가 작은것이 우선, 그래도 같으면 c 좌표가 작은 것이 우선
+ *          - 선택된 정사각형은 시계방향 90도 회전하며 회전된 벽은 내구도 1씩 깎임
+ *      3. 위 과정을 K 초동안 반복. 만약 K 초 전에 모든 참가자가 탈출에 성공하면 게임이 끝난다.
+ *
+ * - 정답 : 모든 참가자들의 이동 거리 합, 출구 좌표를 출력
+ */
+public class 메이즈러너 {
 
     static int N, M, K, moveCnt;
-    static int[][] maze;        // 0 : 빈칸, -1 ~ -9 : 벽 내구도를 나타낸다, -10 : 출구,  1 이상의 자연수 해당 칸에 존재하는 참가자 수
+    static int[][] maze;
 
-    // 참가자 이동 가능 우선 순위 델타 배열 - 상 하 좌 우
-    static int[] sDx = {-1, 1, 0, 0};
-    static int[] sDy = {0, 0, -1, 1};
+    static final int[] dx = {-1, 1, 0, 0};
+    static final int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws Exception {
+    static void input() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -27,12 +53,13 @@ public class 메이즈_러너 {
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                maze[i][j] = -Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                maze[i][j] = -num;
             }
         }
 
-        // 참가자 입력
-        for (int i = 1; i <= M; i++) {
+        // 사람 입력
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
             int x = Integer.parseInt(st.nextToken());
@@ -47,60 +74,54 @@ public class 메이즈_러너 {
         int y = Integer.parseInt(st.nextToken());
         maze[x][y] = -10;
 
-        // 게임 진행
-        solve();
-
-        // 정답 출력
-        int[] exit = findExit();
-        System.out.println(moveCnt);
-        System.out.println(exit[0] + " " + exit[1]);
-
     }
+    public static void main(String[] args) throws Exception {
+        input();
 
-    static void solve() {
-        while (K-- > 0) {
-            // 참가자 이동
-            moveParticipants();
+        for (int i = 1; i <= K; i++) {
+            movePerson();
 
-            // 게임 종료 여부
             if (isFinished()) break;
 
-            // 회전 시키기
-            round();
+            rotate();
             printBoard(maze);
         }
+
+        int[] exit = findExit();
+
+        System.out.println(moveCnt);
+        System.out.println(exit[0] + " " + exit[1]);
     }
 
-    // 모든 참가자 한칸씩 이동
-    static void moveParticipants() {
-        int[][] newMaze = new int[N+1][N+1];
+    static void movePerson() {
+        int[][] tmpMaze = new int[N + 1][N + 1];
         int[] exit = findExit();
         int ex = exit[0];
         int ey = exit[1];
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                // 해당 좌표에 벽이나 출구가 있다면 그대로 복사
+                // 벽이나 출구면 그대로 복사
                 if (maze[i][j] < 0) {
-                    newMaze[i][j] = maze[i][j];
+                    tmpMaze[i][j] = maze[i][j];
                     continue;
                 }
 
                 // 빈 공간이면 continue
                 if (maze[i][j] == 0) continue;
 
-                // 현재 참가자 - 출구 거리
+                // 현재 사람에서 출구까지 거리
                 int curDist = Math.abs(i - ex) + Math.abs(j - ey);
                 int minDist = curDist;
                 int minX = 0;
                 int minY = 0;
 
                 for (int d = 0; d < 4; d++) {
-                    int nx = i + sDx[d];
-                    int ny = j + sDy[d];
+                    int nx = i + dx[d];
+                    int ny = j + dy[d];
 
-                    if (nx < 1 || ny < 1 || nx > N || ny > N) continue;         // 격자 벗어나면 continue
-                    if (-9 <= maze[nx][ny] && maze[nx][ny] <= -1) continue;     // 벽이면 continue
+                    if (isNotBoundary(nx, ny)) continue;
+                    if (-9 <= maze[nx][ny] && maze[nx][ny] <= -1) continue;
 
                     int dist = Math.abs(nx - ex) + Math.abs(ny - ey);
                     if (minDist > dist) {
@@ -112,32 +133,31 @@ public class 메이즈_러너 {
 
                 // 이동 불가라면
                 if (minDist == curDist) {
-                    newMaze[i][j] += maze[i][j];        // 그대로 유지
+                    tmpMaze[i][j] += maze[i][j];     // 그대로 유지
                     continue;
                 }
 
-                moveCnt += maze[i][j];      // 참가자 이동 횟수 증가
+                moveCnt += maze[i][j];
 
-                // 출구에 도달했다면
+                // 출구라면
                 if (maze[minX][minY] == -10) continue;
 
-                newMaze[minX][minY] += maze[i][j];
+                tmpMaze[minX][minY] += maze[i][j];
             }
         }
 
-        // 미로에 복사
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                maze[i][j] = newMaze[i][j];
+                maze[i][j] = tmpMaze[i][j];
             }
         }
-
     }
 
-    // 선택된 정사각형 시계방향 회전
-    static void round() {
-        // 정사각형 크기 결정
-        // 정사각형 크기는 출구와 가장 가까운 참가자 간의 거리가 된다.
+    static boolean isNotBoundary(int x, int y) {
+        return !(1 <= x && x <= N && 1 <= y && y <= N);
+    }
+
+    static void rotate() {
         int minDist = Integer.MAX_VALUE;
         int[] exit = findExit();
         int ex = exit[0];
@@ -145,15 +165,14 @@ public class 메이즈_러너 {
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (maze[i][j] <= 0) continue;          // 참가자가 없다면 continue
+                if (maze[i][j] <= 0) continue;
 
                 int dist = Math.max(Math.abs(i - ex), Math.abs(j - ey));
                 minDist = Math.min(minDist, dist);
             }
         }
 
-        // 정사각형 위치 결정
-        int minX = 0;               // 정사각형의 좌상단 좌표가 될 minX, minY
+        int minX = 0;
         int minY = 0;
         for (int i = 1; i <= N - minDist; i++) {
             for (int j = 1; j <= N - minDist; j++) {
@@ -173,7 +192,7 @@ public class 메이즈_러너 {
                     break;
                 }
             }
-            // 반복문 탈출조건
+
             if (minX != 0) break;
         }
 
@@ -181,12 +200,10 @@ public class 메이즈_러너 {
         System.out.println("minY = " + minY);
         System.out.println("minDist = " + minDist);
         printBoard(maze);
-        // 선택된 정사각형 회전 시키기
-        roundSquare(minX, minY, minDist);
+        rotateMaze(minX, minY, minDist);
     }
 
-    static void roundSquare(int x, int y, int d) {
-        // 격자가 1 부터 시작하고 좌상단이 맨 끝줄일 경우를 대비하여 최소 12 사이즈는 필요
+    static void rotateMaze(int x, int y, int d) {
         int[][] a = new int[12][12];
         int[][] b = new int[12][12];
 
@@ -199,10 +216,7 @@ public class 메이즈_러너 {
         int n = d + 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                // 벽 내구도
-                if (-9 <= a[i][j] && a[i][j] <= -1) {
-                    a[i][j]++;
-                }
+                if (-9 <= a[i][j] && a[i][j] <= -1) a[i][j]++;
                 b[j][n + 1 - i] = a[i][j];
             }
         }
@@ -214,7 +228,6 @@ public class 메이즈_러너 {
         }
     }
 
-    // 게임 종료 여부 확인
     static boolean isFinished() {
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
@@ -225,15 +238,13 @@ public class 메이즈_러너 {
         return true;
     }
 
-    // 출구 찾기
     static int[] findExit() {
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (maze[i][j] == -10) {
-                    return new int[]{i, j};
-                }
+                if (maze[i][j] == -10) return new int[]{i, j};
             }
         }
+
         return null;
     }
 
