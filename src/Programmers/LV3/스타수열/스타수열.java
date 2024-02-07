9f6f4d3e420652ec721c492a8e070fccaa90dd0d
seoyoseb <tjdytpq0310@gmail.com>
2024-02-07 16:39:54 +0900

    :sparkles: 스타 수열 LV3

---
+++ src/Programmers/LV3/스타수열/스타수열.java
@@ -0,0 +1,32 @@
+package Programmers.LV3.스타수열;
+
+public class 스타수열 {
+
+    class Solution {
+        public int solution(int[] a) {
+            int answer = 0;
+
+            int[] cnt = new int[a.length];
+
+            for (int i = 0; i < a.length; i++) {
+                cnt[a[i]]++;
+            }
+
+            for (int i = 0; i < cnt.length; i++) {
+                if (cnt[i] <= answer) continue;
+
+                int ans = 0;
+                for (int j = 0; j < a.length - 1; j++) {
+                    if (a[j] != a[j+1] && (i == a[j] || i == a[j+1])) {
+                        ans++;
+                        j++;
+                    }
+                }
+
+                answer = Math.max(answer, ans);
+            }
+
+            return answer * 2;
+        }
+    }
+}
